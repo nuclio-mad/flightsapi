88e2b5752887ebb037232800ec71d2a1d0b2a7a8
Fernando Calvo <fernando@atani.com>
2022-05-13 15:15:49 +0200

    resolve get passenger routes

--- routes/passenger.js
+++ routes/passenger.js
@@ -1,11 +1,15 @@
 const express = require('express')
+const fs = require('fs')
+const path = require('path')
 
 const passengerRouter = express.Router()
 
 passengerRouter.route('/')
-  .get((req, res) => {
+  .get(async (req, res) => {
     console.log('return passengers')
-    res.json([])
+    const passengersFile = await fs.promises.readFile(__dirname + '/../data/passengers.json', 'utf-8')
+    const passengers = JSON.parse(passengersFile)
+    res.json(passengers)
   })
   .post((req, res) => {
     console.log('create passenger')
@@ -13,9 +17,21 @@ passengerRouter.route('/')
   })
 
 passengerRouter.route('/:passengerId')
-  .get((req, res) => {
-    console.log('return one passenger')
-    res.json({})
+  .get(async (req, res) => {
+    const passengerId = Number(req.params.passengerId)
+    const passengersFile = await fs.promises.readFile(__dirname + '/../data/passengers.json', 'utf-8')
+    const passengers = JSON.parse(passengersFile)
+
+    const flight = passengers.find((flight) => {
+      return flight.id === passengerId
+    })
+
+    if (flight) {
+      return res.json(flight)
+    }
+    res.status(404).json({
+      error: 'passenger not found'
+    })
   })
   .put((req, res) => {
     console.log('update one passenger')
